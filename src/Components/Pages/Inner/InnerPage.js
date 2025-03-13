c892d17d99ab1b798576387f44a93630c54a096e
ItzVakko <itzvakkoofficial@gmail.com>
2025-03-13 16:42:22 +0400

    added messenger

--- src/Components/Pages/Inner/InnerPage.js
+++ src/Components/Pages/Inner/InnerPage.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import SideBarWrapper from "./SideBar/SideBarWrapper";
 import MainWrapper from "./Main/MainWrapper";
+import { FacebookProvider, CustomChat } from "react-facebook";
 
 const InnerPage = () => {
   return (
@@ -11,6 +12,10 @@ const InnerPage = () => {
 
       {/* Here is Main Wrapper rendered */}
       <MainWrapper />
+
+      <FacebookProvider appId="2126083514486740" chatSupport>
+        <CustomChat pageId="576324402235215" minimized="true" />
+      </FacebookProvider>
     </Box>
   );
 };
